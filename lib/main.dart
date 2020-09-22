8af49f6ab07e016958dc9acd6a5f42586f628e48
hakimamg <hakimdu9347@gmail.com>
2020-09-22 13:40:33 +0200

    finish home design player

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(PlayerHome());
@@ -29,8 +30,77 @@ class _Player extends State<Player> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Player Music"),
-        backgroundColor: Colors.black54,
+        backgroundColor: Colors.black87,
         centerTitle: false,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+            color: Colors.black54
+        ),
+        child: Center(
+          child: Column(
+            children: [
+              Card(
+                color: Colors.transparent,
+                elevation: 10.0,
+                margin: EdgeInsets.only(top: 20.0, left: 30.0, right: 30.0),
+                child: Image.asset('assets/un.jpg',
+                    fit: BoxFit.fitHeight),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 20.0),
+                child: Text(
+                  "Intro Swift Course",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 15.0),
+                child: Text(
+                  "AmgIndustries",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 17,
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.fast_rewind),
+                    onPressed: () {
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.play_arrow),
+                    iconSize: 80.0,
+                    onPressed: () {
+
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.skip_next),
+                    onPressed: () {
+
+                    },
+                  ),
+                ],
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10.0),
+                child: Slider(
+                  value: 0.0,
+                  max: 100.0,
+                  mouseCursor: MouseCursor.defer,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
