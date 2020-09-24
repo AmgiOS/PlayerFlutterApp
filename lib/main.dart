3a6d2a913a52a5c6ce20db27f346bdbe0f6345fd
hakimamg <hakimdu9347@gmail.com>
2020-09-24 11:27:02 +0200

    set list property with init state and created text method
    Le monde chico igo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:player_flutter/Music.dart';
 
 void main() {
   runApp(PlayerHome());
@@ -25,6 +26,21 @@ class Player extends StatefulWidget {
 
 class _Player extends State<Player> {
 
+  // Properties
+
+  List<Music> songList = [
+    Music("Theme Swift", "Codabee", "assets/un.jpg", "https://codabee.com/wp-content/uploads/2018/06/un.mp3"),
+    Music("Second Theme", "Eikichi", "assets/deux.jpg", "https://codabee.com/wp-content/uploads/2018/06/deux.mp3")
+  ];
+
+  Music currentMusic;
+
+  @override
+  void initState() {
+    super.initState();
+    currentMusic = songList[0];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,23 +65,11 @@ class _Player extends State<Player> {
               ),
               Container(
                 margin: EdgeInsets.only(top: 20.0),
-                child: Text(
-                  "Intro Swift Course",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 25,
-                  ),
-                ),
+                child: setText(currentMusic.title, 25.0),
               ),
               Container(
                 margin: EdgeInsets.only(top: 15.0),
-                child: Text(
-                  "AmgIndustries",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 17,
-                  ),
-                ),
+                child: setText(currentMusic.artist, 17.0),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -83,7 +87,7 @@ class _Player extends State<Player> {
                     },
                   ),
                   IconButton(
-                    icon: Icon(Icons.skip_next),
+                    icon: Icon(Icons.fast_forward),
                     onPressed: () {
 
                     },
@@ -95,12 +99,24 @@ class _Player extends State<Player> {
                 child: Slider(
                   value: 0.0,
                   max: 100.0,
-                  mouseCursor: MouseCursor.defer,
+                  mouseCursor: MouseCursor.defer, onChanged: (double value) {
+
+                },
                 ),
               )
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Text setText(String title, double fontSize) {
+    return Text(
+      title,
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: fontSize,
       ),
     );
   }
